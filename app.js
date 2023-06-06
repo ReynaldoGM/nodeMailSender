bd87dca2015671175b320fd09d54bcc9f2a8149b
ReynaldoGM <reynaldogm95@gmail.com>
2023-06-06 10:21:18 -0500

    fix cors

--- app.js
+++ app.js
@@ -13,9 +13,7 @@ app.use(bodyParser.urlencoded({
   extended: false
 }));
 
-app.use(cors({
-    origin: ['http://localhost:4200', 'https://static-web-transport.onrender.com/']
-}));
+app.use(cors());
 
 // app.use((req, res, next) => {
 //   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
