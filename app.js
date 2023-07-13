68b65f49eaba12d6ae250cfb373740eae78450ca
ReynaldoGM <reynaldogm95@gmail.com>
2023-07-13 02:08:17 -0500

    Added sendEmailContact and pase the correct data

--- app.js
+++ app.js
@@ -42,6 +42,7 @@ const jsonResponse = `{
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
+
     user: process.env.ONLINE_TRANSPORT_MAIL,
     pass: process.env.MAIL_PASS
   }
@@ -80,8 +81,10 @@ app.get('/', (req, res) => {
 
 
 app.post('/sendEmail', (req, res) => {
+  console.log(req.body.text.inputName);
   mailOptions.subject = req.body.subject;
-  mailOptions.text = req.body.text;
+  mailOptions.text = JSON.stringify(req.body.text);
+  mailOptions.html = `<html><head><style>table{border-collapse:collapse;}th,td{border:1pxsolidblack;padding:8px;}</style></head><body><h1>New Driver </h1><table><tr><th>Name</th><td>${req.body.text.inputName}</td></tr><tr><th>Address</th><td>${req.body.text.inputAdress}</td></tr><tr><th>Email</th><td>${req.body.text.inputEmail}</td></tr><tr><th>Phone</th><td>${req.body.text.inputPhone}</td></tr><tr><th>Experience</th><td>${req.body.text.inputExperience}</td></tr><tr><th>License</th><td>${req.body.text.inputLicense}</td></tr><tr><th>YOL</th><td>${req.body.text.inputYol}</td></tr><tr><th>CurrentCompany</th><td>${req.body.text.inputCurrentCompany}</td></tr><tr><th>Routes</th><td>${req.body.text.inputRoutes}</td></tr></table></body></html>`;
   console.log(process.env.ONLINE_TRANSPORT_MAIL);
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
@@ -96,6 +99,24 @@ app.post('/sendEmail', (req, res) => {
   console.log(req.body);
   res.send(JSON.parse(jsonResponse));
 })
+
+app.post('/sendEmailContact', (req, res) => {
+  mailOptions.subject = req.body.subject;
+  mailOptions.text = JSON.stringify(req.body.text);
+  mailOptions.html = `<html><head><style>table{border-collapse:collapse;}th,td{border:1pxsolidblack;padding:8px;}</style></head><body><h1>New Contact</h1><table><tr><th>Name</th><td>${req.body.text.inputName}</td></tr><tr><th>Company</th><td>${req.body.text.inputCompany}</td></tr><tr><th>Email</th><td>${req.body.text.inputEmail}</td></tr><tr><th>Phone</th><td>${req.body.text.inputPhone}</td></tr><tr><th>Message</th><td>${req.body.text.inputMessage}</td></tr></table></body></html>`;
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      logger.error(error);
+      console.log('Error occurred:', error);
+    } else {
+      logger.info(info.response);
+      console.log('Email sent:', info.response);
+    }
+  });
+  res.send(JSON.parse(jsonResponse));
+
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
